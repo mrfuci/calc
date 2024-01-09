3f3a1ae5a62ce8930afed3ae1081650e0d7d8166
Jan Nypl <nyplj22@spstrutnov.cz>
2024-01-09 11:25:55 +0100

    idfk it dont even work

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -24,5 +24,135 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void procent(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CE(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void C(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Smazat(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_15(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_16(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_17(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_18(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_19(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_20(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_21(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_22(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_23(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void stisk(object sender, RoutedEventArgs e)
+        {
+               
+        }
     }
 }
