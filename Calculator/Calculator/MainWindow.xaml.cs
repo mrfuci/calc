b37083520e3427b5a78d08448c6731538ee3c751
Jan Nypl <nyplj22@spstrutnov.cz>
2024-01-23 11:35:23 +0100

    už to něco dělá

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,29 +22,117 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double firstNumber;
+        private double secondNumber;
+        private Operator currentOperator;
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        public enum Operator
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide,
+        }
         private void stisk(object sender, RoutedEventArgs e)
         {
-               
-        }
+            var btn = sender as Button;
 
+            if (label.Text != "0")
+            {
+                label.Text = $"{label.Text}{btn.Content}";
+            }
+            else
+            {
+                label.Text = btn.Content.ToString();
+            }
+        }
+        
         private void tlacitko_percent(object sender, RoutedEventArgs e)
         {
-            label.Text += "%";
+            secondNumber = double.Parse(label.Text);
+            label.Text = GetResult(firstNumber, currentOperator, secondNumber);
         }
+        
+        
+        private string GetResult(double firstNumber, Operator currentOperator, double secondNumber)
+        {
+            if (currentOperator == Operator.Add)
+            {
+                return (firstNumber + secondNumber).ToString();
+            }
+            else if (currentOperator == Operator.Subtract)
+            {
+                return (firstNumber - secondNumber).ToString();
+            }
+            else if (currentOperator == Operator.Multiply)
+            {
+                return (firstNumber * secondNumber).ToString();
+            }
+            else if (currentOperator == Operator.Divide)
+            {
+                return (firstNumber / secondNumber).ToString();
+            }
+            else return "0";
+        }            
 
         private void tlacitko_CE_Click(object sender, RoutedEventArgs e)
         {
-            label.Text = "";
+            firstNumber = 0;
+            secondNumber = 0;
+            currentOperator = 0;
+            label.Text = firstNumber + "0";
+        }
+        private void tlacitko_rovnase_Click(object sender, RoutedEventArgs e)
+        {
+            secondNumber = double.Parse(label.Text);
+            label.Text = GetResult(firstNumber,currentOperator, secondNumber);
+        }
+
+        private void tlacitko_plus_Click(object sender, RoutedEventArgs e)
+        {
+            currentOperator = Operator.Add;
+            firstNumber = double.Parse(label.Text);
+            label.Text = firstNumber + "+";
+        }
+
+        private void tlacitko_minus_Click(object sender, RoutedEventArgs e)
+        {
+            currentOperator = Operator.Subtract;
+            firstNumber = double.Parse(label.Text);
+            label.Text = firstNumber + "-";
+        }
+
+        private void tlacitko_krat_Click(object sender, RoutedEventArgs e)
+        {
+            currentOperator = Operator.Multiply;
+            firstNumber = double.Parse(label.Text);
+            label.Text=firstNumber + "*";
+        }
+        private void tlacitko_prehozeni_plus_minus_Click(object sender, RoutedEventArgs e)
+        {
+            label.Text = (double.Parse(label.Text) * -1).ToString();
         }
 
         private void tlacitko_C_Click(object sender, RoutedEventArgs e)
         {
-            label.Text = "";
+            label.Text = firstNumber + "";
+        }
+        private void tlacitko_carka_Click(object sender, RoutedEventArgs e)
+        {
+            if (label.Text.IndexOf(".") < 0 ) 
+            {
+            label.Text += ".";
+            }
+        }
+        private void tlacitko_deleno_Click(object sender, RoutedEventArgs e)
+        {
+            currentOperator = Operator.Divide;
+            firstNumber= double.Parse(label.Text);
+            label.Text=firstNumber + "/";
         }
 
         private void tlacitko_backspace_Click(object sender, RoutedEventArgs e)
@@ -64,12 +154,6 @@ namespace Calculator
         {
 
         }
-
-        private void tlacitko_deleno_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void tlacitko_0_Click(object sender, RoutedEventArgs e)
         {
             label.Text += "0" ;
@@ -80,7 +164,7 @@ namespace Calculator
         }
         private void tlacitko_2_Click(object sender, RoutedEventArgs e)
         {
-            label.Text += "2";
+            label.Text += "2" ;
         }
         private void tlacitko_3_Click(object sender, RoutedEventArgs e)
         {
@@ -109,36 +193,6 @@ namespace Calculator
         private void tlacitko_9_Click(object sender, RoutedEventArgs e)
         {
             label.Text += "9";
-        }
-
-        private void tlacitko_prehozeni_plus_minus_Click(object sender, RoutedEventArgs e)
-        {
-            label.Text = "-" + label.Text;
-        }
-
-        private void tlacitko_carka_Click(object sender, RoutedEventArgs e)
-        {
-            label.Text += ",";
-        }
-
-        private void tlacitko_rovnase_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void tlacitko_plus_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void tlacitko_minus_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void tlacitko_krat_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
